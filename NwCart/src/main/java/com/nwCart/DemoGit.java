379d12d8fc332680d577cdbf8dc175fe6fac31ab
nsonali <nsonali21@gmail.com>
2023-11-25 09:23:50 +0530

    kssue4

---
+++ NwCart/src/main/java/com/nwCart/DemoGit.java
@@ -0,0 +1,12 @@
+package com.nwCart;
+
+public class DemoGit {
+
+	public static void main(String[] args) {
+		System.out.println("2nd try for arijit");
+		System.out.println("New  try for arijit");
+		
+		System.out.println("New  try for arijit");
+	}
+
+}
