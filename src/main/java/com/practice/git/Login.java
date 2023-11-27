958f17ac03ba7ce5a795b60281be37104c5cdaf6
nsonali <nsonali21@gmail.com>
2023-11-27 18:03:37 +0530

    2nd owntry

---
+++ src/main/java/com/practice/git/Login.java
@@ -0,0 +1,11 @@
+package com.practice.git;
+
+public class Login {
+
+	public static void main(String[] args) {
+    System.out.println("I am in Login Page");
+    System.out.println("I am in Login Page");
+
+	}
+
+}
